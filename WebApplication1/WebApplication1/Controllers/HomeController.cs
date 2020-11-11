238b0a870e0ede3a001862206a6da4323ebc1d1d
Larissa Sousa <larissa.sousa@bandtec.com.br>
2020-11-11 12:15:52 -0300

    Criação e manipulação das Razors

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult About() {
+            ViewData["Message"] = "Salles Web MVC App from C# course";
+            ViewData["Aluno"] = "Larissa Custódio de Sousa";
+            return View();
+        }
+
+        public IActionResult Contact() {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new WebApplication1.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
