902903645e7164bd1ef357d2c8d075e05a83c30c
Larissa Sousa <larissa.sousa@bandtec.com.br>
2020-11-12 11:57:48 -0300

    Adicionando CRUD Scaffolding

--- WebApplication1/WebApplication1/Models/Department.cs
+++ WebApplication1/WebApplication1/Models/Department.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace WebApplication1.Models {
     public class Department {
+
         public int Id { get; set; }
         public string Name { get; set; }
     }
